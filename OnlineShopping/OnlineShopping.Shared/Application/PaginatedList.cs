4f26f43b75408cd798875191fb2b5b7e9546e40f
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-10-20 20:09:12 +0300

    PaginatedList and IRepository added to Shared

---
+++ OnlineShopping/OnlineShopping.Shared/Application/PaginatedList.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShopping.Shared.Domain.Entities;
+
+public class PaginatedList<T>
+{
+    public IReadOnlyCollection<T> Items { get; }
+    public int PageNumber { get; }
+    public int TotalPages { get; }
+    public int TotalCount { get; }
+
+    public PaginatedList(IReadOnlyCollection<T> items, int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        TotalPages = (int) Math.Ceiling(items.Count / (double) pageSize);
+        TotalCount = items.Count;
+        Items = items;
+    }
+}
---
+++ OnlineShopping/OnlineShopping.Shared/Infrastructure/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShopping.Shared.Infrastructure
+{
+    public interface ISharedRepository<T>
+    {
+        IQueryable<T> GetAllAsQueryable();
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<int> AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void AddRangeWithoutSaveAsync(IEnumerable<T> entities);
+
+        Task RemoveAsync(T entity, CancellationToken cancellationToken);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        Task RemoveRangeWithoutSaveAsync(IEnumerable<T> entities);
+
+        Task SaveAsync();
+    }
+}
