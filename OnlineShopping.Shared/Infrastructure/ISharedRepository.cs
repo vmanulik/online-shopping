81d1a2e97f2ca78daad9385dc28b79f8812ebc56
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-10-20 22:39:30 +0300

    Shared Repository simplified

--- OnlineShopping.Shared/Infrastructure/ISharedRepository.cs
+++ OnlineShopping.Shared/Infrastructure/ISharedRepository.cs
@@ -6,14 +6,10 @@
         Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
         Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
 
-        Task<int> AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
         Task AddRangeAsync(IEnumerable<T> entities);
-        void AddRangeWithoutSaveAsync(IEnumerable<T> entities);
 
         Task RemoveAsync(T entity, CancellationToken cancellationToken);
         Task RemoveRangeAsync(IEnumerable<T> entities);
-        Task RemoveRangeWithoutSaveAsync(IEnumerable<T> entities);
-
-        Task SaveAsync();
     }
 }
