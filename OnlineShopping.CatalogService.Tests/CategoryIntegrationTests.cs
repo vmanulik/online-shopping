af4e92d924e91080834f3d15ab03cb7f79c498d8
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-10-21 15:41:48 +0300

    Integration tests added

---
+++ OnlineShopping.CatalogService.Tests/CategoryIntegrationTests.cs
@@ -0,0 +1,51 @@
+using OnlineShopping.CartService.Domain.Entities;
+using OnlineShopping.CatalogService.API;
+using OnlineShopping.CatalogService.Application.Categories.Commands;
+using OnlineShopping.CatalogService.Tests.Configuration;
+using System.Net.Http.Json;
+
+namespace OnlineShopping.CatalogService.Tests
+{
+    [TestFixture]
+    public class CategoryIntegrationTests
+    {
+        private CustomWebApplicationFactory<Program> _factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new CustomWebApplicationFactory<Program>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _factory.Dispose();
+        }
+
+        [Test]
+        public async Task Add_ShouldBeStoredAndRetrieved()
+        {
+            using var client = _factory.CreateClient();
+
+            var response1 = await client.GetFromJsonAsync<List<Category>>("/category");
+            Assert.IsNotNull(response1);
+            Assert.That(response1.Count, Is.EqualTo(0));
+
+            var request = new CreateCategoryCommand("Category 1", null, null);
+            var response2 = await client.PostAsJsonAsync("/category", request);
+            response2.EnsureSuccessStatusCode();
+
+            var response3 = await client.GetFromJsonAsync<List<Category>>("/category");
+            Assert.IsNotNull(response3);
+            Assert.That(response3.Count, Is.EqualTo(1));
+
+            var response4 = await client.DeleteAsync("/category/1");
+            response4.EnsureSuccessStatusCode();
+
+            var response5 = await client.GetFromJsonAsync<List<Category>>("/category");
+            Assert.IsNotNull(response5);
+            Assert.That(response5.Count, Is.EqualTo(0));
+        }
+    }
+}
---
+++ OnlineShopping.CatalogService.Tests/Configuration/CustomWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShopping.CatalogService.Infrastracture.Persistence;
+using System.Data.Common;
+
+namespace OnlineShopping.CatalogService.Tests.Configuration;
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<CatalogServiceDbContext>));
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbConnection));
+            services.Remove(dbConnectionDescriptor);
+
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<CatalogServiceDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
