00c2f66d347a21b65f938442cc9c45a29aa5c20a
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-10-21 08:48:22 +0300

    Domain model added

---
+++ OnlineShopping.CatalogService.Domain/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShopping.Shared.Domain.Entities;
+
+namespace OnlineShopping.CartService.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string Name { get; init; }
+    public string? ImageUrl { get; init; }
+
+    public int? ParentCategoryId { get; set; }
+    public Category ParentCategory { get; private set; }
+
+    public Category() { }
+
+    public Category(string name, int parentCategoryId)
+    {
+        Name = name;
+        ParentCategoryId = parentCategoryId;
+    }
+}
---
+++ OnlineShopping.CatalogService.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using OnlineShopping.Shared.Domain.Entities;
+
+namespace OnlineShopping.CartService.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; init; }
+    public string? ImageUrl { get; init; }
+    public string? ImageDescription { get; init; }
+    public decimal Price { get; init; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; private set; }
+
+    public Product() { }
+
+    public Product(string name, decimal price, int categoryId)
+    {
+        Name = name;
+        Price = price;
+        CategoryId = categoryId;
+    }
+}
