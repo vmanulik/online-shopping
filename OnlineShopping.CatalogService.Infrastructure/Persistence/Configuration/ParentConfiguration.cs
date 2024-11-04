019a42375366c5fc9ba1d9e8c29c45a45d758a84
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-11-04 11:40:43 +0200

    Configuration added

---
+++ OnlineShopping.CatalogService.Infrastructure/Persistence/Configuration/ParentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShopping.CartService.Domain.Entities;
+
+namespace OnlineShopping.CatalogService.Infrastracture.Persistence.Configuration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder
+            .HasOne(x => x.Category)
+            .WithMany(t => t.Products)
+            .HasForeignKey(e => e.CategoryId);
+    }
+}
+
