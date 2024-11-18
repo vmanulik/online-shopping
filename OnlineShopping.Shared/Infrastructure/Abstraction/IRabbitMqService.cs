e2efe2fd5168b3f7edb2fbe00fc90141ce2d6330
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-11-18 02:36:50 +0200

    IRabbitMqService removed

--- OnlineShopping.Shared/Infrastructure/Abstraction/IRabbitMqService.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineShopping.Shared.Domain.Entities;
-
-namespace OnlineShopping.Shared.Infrastructure.Abstraction
-{
-    public interface IRabbitMqService
-    {
-        void SendMessage(IntegrationEvent message);
-    }
-}
