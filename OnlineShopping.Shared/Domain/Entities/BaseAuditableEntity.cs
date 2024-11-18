ba33bd959c80774fe12261527417bf98a2f329de
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-11-18 09:49:41 +0200

    BaseAuditableEntity added

---
+++ OnlineShopping.Shared/Domain/Entities/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopping.Shared.Domain.Entities;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTime Created { get; set; }
+
+    public DateTime? LastModified { get; set; }
+
+    public int Version { get; set; }
+}
