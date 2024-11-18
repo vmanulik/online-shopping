a9835d4b7bc5baa0e30d75d6a02ef1cfac73e70a
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-11-18 02:31:55 +0200

    IRabbitmqService, IntegrationEvent added

---
+++ OnlineShopping.Shared/Domain/Entities/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopping.Shared.Domain.Entities;
+
+public class IntegrationEvent
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Data { get; set; }
+}
---
+++ OnlineShopping.Shared/Infrastructure/Abstraction/IRabbitMqService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineShopping.Shared.Domain.Entities;
+
+namespace OnlineShopping.Shared.Infrastructure.Abstraction
+{
+    public interface IRabbitMqService
+    {
+        void SendMessage(IntegrationEvent message);
+    }
+}
