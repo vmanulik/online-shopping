2046fea5f717acdbb8e0d79437cda4819838b5e1
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-11-22 15:37:17 +0200

    Domain events added to shared lib

--- OnlineShopping.Shared/Domain/Entities/BaseEntity.cs
+++ OnlineShopping.Shared/Domain/Entities/BaseEntity.cs
@@ -1,13 +1,36 @@
-﻿namespace OnlineShopping.Shared.Domain.Entities;
+﻿using OnlineShopping.Shared.Domain.Events;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShopping.Shared.Domain.Entities;
 
 public abstract class BaseEntity
 {
+    private readonly List<BaseEvent> _domainEvents = new List<BaseEvent>();
+
     public int Id { get; init; }
+
+    [NotMapped]
+    public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
 
     protected BaseEntity() { }
 
     protected BaseEntity(int id)
     {
         Id = id;
+    }
+
+    public void AddDomainEvent(BaseEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void RemoveDomainEvent(BaseEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
     }
 }
---
+++ OnlineShopping.Shared/Domain/Events/BaseEvent.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace OnlineShopping.Shared.Domain.Events;
+
+public abstract class BaseEvent : INotification { }
