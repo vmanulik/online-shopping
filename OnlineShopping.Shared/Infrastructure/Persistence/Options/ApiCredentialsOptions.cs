ac96f93fe34e19fdc73c8a893f34cca1fd4779f1
Viacheslav Manulik <viacheslav_manulik@epam.com>
2024-12-02 19:10:11 +0200

    ApiCredentialsOptions introduced

---
+++ OnlineShopping.Shared/Infrastructure/Persistence/Options/ApiCredentialsOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopping.Shared.Infrastructure.Persistence.Options;
+
+public class ApiCredentialsOptions
+{
+    public string Url { get; init; }
+    public int Port { get; init; }
+    public string ClientId { get; init; }
+    public string ClientSecret { get; init; }
+}
--- OnlineShopping.Shared/Infrastructure/Persistence/Options/RabbitMqOptions.cs
+++ OnlineShopping.Shared/Infrastructure/Persistence/Options/RabbitMqOptions.cs
@@ -1,11 +1,7 @@
 ﻿namespace OnlineShopping.Shared.Infrastructure.Persistence.Options;
 
-public class RabbitMqOptions
+public class RabbitMqOptions : ApiCredentialsOptions
 {
-    public string Url { get; init; }
-    public int Port { get; init; }
     public string VirtualHost { get; init; }
-    public string UserName { get; init; }
-    public string Password { get; init; }
     public int EventFetchPeriod { get; init; }
 }
